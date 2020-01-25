6370253e91fceaaf6c191b903a02775640833575
Vyankatesh Madgundi <venky.v2@gmail.com>
2020-01-25 21:59:52 +0530

    Added shop data and cart utility

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component.jsx';
+import {addItem } from '../../redux/cart/cart.actions'
 import './collection-item.style.scss';
-const CollectionItem = ({id, name, price, imageUrl}) => (
-<div className='collection-item'>
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl } = item;
+    return (
+    <div className='collection-item'>
     <div
         className='image'
         style={{
@@ -12,7 +17,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-</div>
-);
+        <CustomButton onClick={()=> addItem(item)} inverted> ADD to Cart </CustomButton>
+    </div>
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
