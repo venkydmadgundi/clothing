cbd044c8690aefb9bea73ecffd84e775569cc579
Vyankatesh Madgundi <venky.v2@gmail.com>
2020-01-25 19:15:15 +0530

    Added user types for constant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header-component.jsx';
-import SignInAndSignUpPAge from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 
 
 import {auth, createUserProfileDocument } from './firebase/firebase.utils.js';
@@ -49,18 +49,28 @@ class App extends React.Component {
           <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPAge} />
+          <Route path='/signin' render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/'/>
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
           </Switch>
         </div>
       );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
